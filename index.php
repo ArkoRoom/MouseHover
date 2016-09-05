4c2cc542f1141ea89e6cc3d026306844fb23c549
Arkolips <catel.ludovic@gmail.com>
2016-09-05 15:34:32 +0200

    main function

--- index.php
+++ index.php
@@ -3,13 +3,28 @@
   <head>
     <meta charset="utf-8">
     <title>Mouse Hover</title>
-    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
+    <link rel="stylesheet" href="vendor/bootstrap/dist/css/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
     <link rel="stylesheet" href="css/style.css" media="screen" title="no title" charset="utf-8">
   </head>
   <body>
+    <div class="container">
+      <div class="use-advice">
+        <h2>Mouse Hover</h2>
+        <p>
+          Mouse Hover est un petit script qui permets de révéler une image en passant simplement son curseur sur la zone.<br>
+          Des petits carrés se forment alors permettant de révéler tout ou partie de l'image selon votre souhait.<br>
+          L'image est générée grâce à une variable aléatoire.
+        </p>
+      </div>
+      <div class="col-md-10 col-md-offset-1">
+        <div class="wrap-img">
+          
+        </div>
+      </div>
+    </div>
 
-    <script src="vendor/jquery/jquery.min.js" charset="utf-8"></script>
-    <script src="vendor/bootstrap/js/bootstrap.min.js" charset="utf-8"></script>
+    <script src="vendor/jquery/dist/jquery.min.js" charset="utf-8"></script>
+    <script src="vendor/bootstrap/dist/js/bootstrap.min.js" charset="utf-8"></script>
     <script src="js/script.js" charset="utf-8"></script>
   </body>
 </html>
--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-Commande ECHO activ�e.
+$(function () {
+
+  // On crée le quadrillage
+  var createDiv = new Array();
+  for (var i = 0; i <= 824; i++) {
+    createDiv[i] = $('.wrap-img').append("<div class='div-mouse-hover" + i + " hover-effect'></div>");
+    $('.div-mouse-hover' + i + '').css({
+      'display': 'inline-block',
+      'width': '10px',
+      'height': '10px',
+      'margin-left': '5px',
+      'background-color': "#FFFFFF"
+    });
+  }
+  // On crée l'effet "mouseHover"
+  $('.hover-effect').mouseover(function () {
+    $('.hover-effect').css({
+      'background-color': 'transparent'
+    });
+  });
+});
