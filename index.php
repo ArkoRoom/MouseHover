78db8a2b48dd9e5330e824b68ec91f608ef040e9
Arkolips <catel.ludovic@gmail.com>
2016-09-05 03:32:30 +0200

    add index.php

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-Commande ECHO activ‚e.
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Mouse Hover</title>
+    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
+    <link rel="stylesheet" href="css/style.css" media="screen" title="no title" charset="utf-8">
+  </head>
+  <body>
+
+    <script src="vendor/jquery/jquery.min.js" charset="utf-8"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js" charset="utf-8"></script>
+    <script src="js/script.js" charset="utf-8"></script>
+  </body>
+</html>
